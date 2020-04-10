5cc106b5490ec694e98b8c9213e3dec9db4c6540
meilixiaozhang <redpangci@gmail.com>
2020-04-10 20:58:22 +1000

    relationship with mongoose

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const fruitSchema = new mongoose.Schema({
 const Fruit = mongoose.model("Fruit", fruitSchema);
 
 const fruit = new Fruit({
-  // name: "Peach",
+  name: "Peach",
   rating: 10,
   review: "Peaches are so yummy"
 });
@@ -27,35 +27,66 @@ const fruit = new Fruit({
 
 const personSchema = new mongoose.Schema({
   name: String,
-  age: Number
+  age: Number,
+  favouriteFruit: fruitSchema
 })
 
 const Person = mongoose.model("Person", personSchema);
 
 const person = new Person({
   name: "John",
-  age: 37
+  age: 37,
 })
+
+const pineapple = new Fruit({
+  name: "Pineapple",
+  score: 9,
+  review: "Great fruit"
+})
+
+const mango = new Fruit({
+  name: "Mango",
+  score: 8,
+  review: "Pretty descent fruit"
+})
+
+mango.save()
+
+// pineapple.save()
+
+// const person = new Person({
+//   name: "Mandy",
+//   age: 16,
+//   favouriteFruit: pineapple
+// })
 
 // person.save();
 
-// const kiwi = new Fruit({
-//   name: "kiwi",
-//   score: 10,
-//   review: "The best fruit"
-// });
-//
-// const orange = new Fruit({
-//   name: "orange",
-//   score: 4,
-//   review: "Too sour for me"
-// })
-//
-// const banana = new Fruit({
-//   name: "banana",
-//   score: 3,
-//   review: "Weird texture"
-// })
+Person.updateOne({name: "John"}, {favouriteFruit: mango}, function(err){
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Success");
+  }
+})
+
+const kiwi = new Fruit({
+  name: "kiwi",
+  score: 10,
+  review: "The best fruit"
+});
+
+const orange = new Fruit({
+  name: "orange",
+  score: 4,
+  review: "Too sour for me"
+})
+
+const banana = new Fruit({
+  name: "banana",
+  score: 3,
+  review: "Weird texture"
+})
 
 // Fruit.insertMany([kiwi, orange, banana], function(err){
 //   if (err) {
@@ -65,13 +96,13 @@ const person = new Person({
 //   }
 // })
 
-Person.deleteMany({name: "John"}, function(err){
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Success");
-  }
-})
+// Person.deleteMany({name: "John"}, function(err){
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Success");
+//   }
+// })
 
 // Fruit.find(function(err, fruits){
 //   if (err) {
@@ -94,6 +125,14 @@ Person.deleteMany({name: "John"}, function(err){
 // })
 
 // Fruit.deleteOne({name: "Peach"}, function(err){
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Success");
+//   }
+// })
+
+// Person.deleteOne({name: "Mandy"}, function(err){
 //   if (err) {
 //     console.log(err);
 //   } else {
